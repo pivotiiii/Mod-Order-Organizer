4df91bc3d69332f54b32befd56d5f116a4f967a5
pivotiiii <boysilver7@googlemail.com>
2016-02-07 22:11:25 +0100

    all files

---
+++ registry_access.py
@@ -0,0 +1,26 @@
+import winreg as _winreg
+
+
+REG_PATH = r"SOFTWARE\Mod Order Organizer"
+
+
+def set_reg(name, value):
+    try:
+        _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, REG_PATH)
+        registry_key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, REG_PATH, 0, 
+                                       _winreg.KEY_WRITE)
+        _winreg.SetValueEx(registry_key, name, 0, _winreg.REG_SZ, value)
+        _winreg.CloseKey(registry_key)
+        return True
+    except WindowsError:
+        return False
+
+def get_reg(name):
+    try:
+        registry_key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, REG_PATH, 0,
+                                       _winreg.KEY_READ)
+        value, regtype = _winreg.QueryValueEx(registry_key, name)
+        _winreg.CloseKey(registry_key)
+        return value
+    except WindowsError:
+        return None
